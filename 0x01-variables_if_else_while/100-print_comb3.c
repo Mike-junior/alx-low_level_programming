fda8e611ef709cfc82e57fb6b8f03a8aee978c03
Mike-junior <agolamikejunior4@gmail.com>
2022-10-19 16:33:48 -0700

    Write a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 /**
  * main - Prints numbers between 00 to 89.
  *
- * Description - program that prints all possible different combinations of two digits
+ * Description -  prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
 
