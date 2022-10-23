aa5a3fb11aafcfa7bd2f18b77524899709830539
Mike-junior <agolamikejunior4@gmail.com>
2022-10-23 07:37:30 -0700

    print number from 0-9 with a newline

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,16 +4,13 @@
  * print_numbers - prints 0-9 and a newline
  * Return: void
  */
-
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = 0; c <= 9; c++)
+	for (i = 0; i < 10; i++)
 	{
-		 _putchar(c + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
-
-
