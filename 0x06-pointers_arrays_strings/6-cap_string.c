fecf8cbbcc5da93729765f6ecc2b76eeadf7e999
Mike-junior <agolamikejunior4@gmail.com>
2022-11-27 11:00:44 -0800

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 char *cap_string(char *x)
 {
 
-	char separator_words[] = {32, 9, '\n', ',' ';', '.', '!', '?', '"',
+	char separator_words[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"',
 		'(', ')', '{', '}' };
 	int len = 13;
 	int a = 0, i;
