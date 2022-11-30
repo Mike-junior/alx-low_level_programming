d0ef83c23762e1fae707ec6e15a0ecdb2a07db47
Mike-junior <agolamikejunior4@gmail.com>
2022-11-29 18:49:35 -0800

    Write a function that prits printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,15 +5,13 @@
  myCfunction- Apply the constructor attribute to myCfunction() so that it
  *   is executed before main()
  */
-void
-myCfunction(void) __attribute__ ((constructor));
+void myCfunction(void) __attribute__ ((constructor));
 
 /**
  * 
  myCfunction- implementation of myCfunction
  */
-void 
-myCfunction(void)
+void myCfunction(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
